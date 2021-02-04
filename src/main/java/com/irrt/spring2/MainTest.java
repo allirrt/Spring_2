d7c2b0caf78e47fe8899942235a5be40261cf8a0
IRRT <zindgg@gmail.com>
2021-02-04 10:38:32 +0300

    Spring. Create project with  XML file

--- src/main/java/com/irrt/spring2/MainTest.java
+++ src/main/java/com/irrt/spring2/MainTest.java
@@ -3,12 +3,36 @@ package com.irrt.spring2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+     //   Music music = context.getBean("musicBean", Music.class);
+     //   MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+
+        musicPlayer.setVolume(10);
+
+        System.out.println();
+
+        System.out.println("Жанр музыки: " + musicPlayer.getName() + ". Уровень громкости: " + musicPlayer.getVolume());
+
+        singleTone(musicPlayer, secondMusicPlayer);
         context.close();
+    }
+    public static void singleTone(MusicPlayer firstMusicPlayer, MusicPlayer secondMusicPlayer){
+        boolean b;
+        boolean b1;
+
+        b = firstMusicPlayer.equals(secondMusicPlayer);
+        System.out.println(b);
+
+        b1 = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(b);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
 
     }
 }
---
+++ src/main/java/com/irrt/spring2/Music.java
@@ -0,0 +1,5 @@
+package com.irrt.spring2;
+
+public interface Music {
+    String getSong();
+}
